31210dc9485efb18ae9f99a942f4068dfeb07c79
M Nakul Kumar <nakul.m.kumar@gmail.com>
2021-08-01 11:34:20 +0530

    Saving a pattern in BeatBox through serialization

--- BeatBox.java
+++ BeatBox.java
@@ -166,5 +166,28 @@ public class BeatBox{
 		} catch(Exception e) {e.printStackTrace();}
 		return event;
 	}
+
+	public class MySendListener implements ActionListener{
+    public void actionPerformed(ActionEvent a){
+        boolean[] checkboxState = new boolean[256];
+
+        for(int i=0;i<256;i++){
+            JCheckBox check =(JCheckBox) checkboxList.get(i);
+            if(check.isSelected()){
+                checkboxState[i]=true;
+            }
+        }
+
+        try{
+            FileOutputStream fileStream = new FileOutputStream(new File("Checkbox.ser"));
+            ObjectOutputStream os = new ObjectOutputStream(fileStream);
+            os.writeObject(checkboxState);
+
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+
+    }
+}
 }
 
