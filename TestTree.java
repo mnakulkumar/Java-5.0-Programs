790029524ad3ab3a23994ea5b96bdf7fdde2c494
M Nakul Kumar <nakul.m.kumar@gmail.com>
2021-09-02 11:34:46 +0530

    TestTree program having books in TreeSet and sorting it with inner class implementing Comparator

--- TestTree.java
+++ TestTree.java
@@ -5,32 +5,37 @@ public class TestTree{
         new TestTree().go();
     }
 
-    public void go(){
-        
-        Book b1 = new Book("How Cats Work");        
-        Book b2 = new Book("Remix your Body");
-        Book b3 = new Book("Finding Emo");
-        Book b4 = new Book("Harry Potter");
+     public class BookCompare implements Comparator<Book>{
+        public int compare(Book one,Book two){
+            return (one.getTitle().compareTo(two.getTitle()));
+        }
+    }
 
-        
+    public void go(){        
 
-        TreeSet<Book> tree = new TreeSet<Book>();
-        tree.add(b1);
-        tree.add(b2);
-        tree.add(b3);
-        tree.add(b4);
+        BookCompare bCompare = new BookCompare();
+        TreeSet<Book> tree = new TreeSet<Book>(bCompare);
+        tree.add(new Book("How Cats Work"));
+        tree.add(new Book("Remix your Body"));
+        tree.add(new Book("Finding Emo"));
+        tree.add(new Book("Harry Potter"));
+        tree.add(new Book("Beauty And The Beast"));
+        tree.add(new Book("Pride And Prejudice"));
         
         System.out.println(tree);
     
     }
 }
 
-class Book implements Comparable<Book>{
+class Book  implements Comparable<Book> {
     private String title;
 
     public int compareTo(Book b){
+        
         return title.compareTo(b.getTitle());
     }
+
+   
 
     public Book(String t) {
         title = t;
