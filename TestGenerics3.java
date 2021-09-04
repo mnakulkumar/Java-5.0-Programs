ae936081e79ad154e7912e86c1f31a8b2f3c015d
M Nakul Kumar <nakul.m.kumar@gmail.com>
2021-09-04 10:39:43 +0530

    Polymorphic collection types using wildcards

---
+++ TestGenerics3.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class TestGenerics3 {
+    public static  void main(String[] args){
+        new TestGenerics3().go();
+    }
+
+    public void go(){
+        ArrayList<Animal> animals = new ArrayList<Animal>();
+        animals.add(new Dog());
+        animals.add(new Cat());
+        animals.add(new Dog());
+        takeAnimals(animals);
+
+        ArrayList<Dog> dogs = new ArrayList<Dog>();
+        dogs.add(new Dog());
+        dogs.add(new Dog());
+        takeAnimals(dogs);
+    }
+
+    public void takeAnimals(ArrayList<? extends Animal> animals){ // with Wildcards <?>  OR other syntax i.e. declaring type parameter once i.e public <T extends Animal> void takeAnimals(ArrayList<T> animals)
+
+        for(Animal a: animals){
+            a.eat();
+        }
+    }
+
+    abstract class Animal {
+        void eat(){
+            System.out.println("animal eating");
+        }
+    }
+
+    class Dog extends Animal{
+        void bark() { }
+    }
+
+    class Cat extends Animal{
+        void meow() { }
+    }
+}
